38133978cb34374840aeb71edd631ab949f5db1d
alsongard <alsongard@gmail.com>
2024-02-01 17:15:46 -0500

    practise

---
+++ Java-Practise/js/practise.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("change");
+const text = document.getElementById("changeTxt");
+
+button.onclick = function myFunc(){
+    text.innerText = "WELCOME TO JAVASCRIPT TRAINING";
+};
