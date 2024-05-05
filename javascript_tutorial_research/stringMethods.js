63412b219e4516fa6a491ce67aace4bdf5e0eca5
alsongard <alsongard@gmail.com>
2024-05-05 09:43:59 -0400

    challenge2

--- javascript_tutorial_research/stringMethods.js
+++ javascript_tutorial_research/stringMethods.js
@@ -7,4 +7,17 @@ function capitalizeString(mystring){
 }
 let new_sentence = capitalizeString(sentence);
 console.log(new_sentence);
+
+const numbers = [1,2,3,4];
+let new_numbers = numbers.map(function(item){
+	return item ** 2;
+})
+console.log(new_numbers);
+
 //map function
+//challenge 2:
+const names = ['john', 'peter', 'susan', 'anna'];
+let new_names = names.map(function(items){
+	return items.charAt(0).toUpperCase() + items.slice(1);
+})
+console.log(new_names);
