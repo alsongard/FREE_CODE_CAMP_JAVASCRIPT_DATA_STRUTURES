01439bcd0e0db37aa1a93b1fa8973ddca3611549
alsongard <alsongard@gmail.com>
2025-02-10 21:33:00 +0200

    calculating remaining calorie fully functional

--- js/calorieCounter.js
+++ js/calorieCounter.js
@@ -58,7 +58,24 @@ function addEntry() {
     const dinnerNumberInputs = document.querySelectorAll("#dinner input[type='number']");
     const snacksNumberInputs = document.querySelectorAll("#snacks input[type='number']");
     const exerciseNumberInputs = document.querySelectorAll("#exercise input[type='number']");
+    const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+
+    if (isError) {
+      return;
+    }
   
+    const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+    const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+    const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+    output.innerHTML = `
+    <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+    <hr>
+    <p>${budgetCalories} Calories Budgeted</p>
+    <p>${consumedCalories} Calories Consumed</p>
+    <p>${exerciseCalories} Calories Burned</p>
+    `;
+  
+    output.classList.remove('hide');
   
   }
   
